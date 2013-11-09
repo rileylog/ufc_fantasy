9f4958b858c4008ab9363fdc2e3a4451a59aa403
Zach Thompson <escapetherobots@gmail.com>
2013-11-09 11:04:43 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UfcFantasy::Application.config.secret_token = 'c8e8ac67f82ec97cf793cdfd9528f6de5f7bd264ed04cb538c9adfd2bae326620cea8f127b124032a06a66bdaad2f94528572af4869e3c440ec177354b586323'
